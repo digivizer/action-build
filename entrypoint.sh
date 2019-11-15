7841d8412a872a478be130649bc5cb56a529b86e
camerongray <868953+camerondgray@users.noreply.github.com>
2019-11-15 14:03:44 -0800

    fail action if the build fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 echo "Building"
 export CONVOX_RACK=$INPUT_RACK
 release=$(convox build --app $INPUT_APP --id)
+if [ -z "$RELEASE" ]
+then
+  echo "Build failed"
+  exit 1
+fi
 echo ::set-output name=release::$release
 echo ::set-env name=RELEASE::$release
