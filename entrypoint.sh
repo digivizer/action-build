121c662184eb714bc97f18695aea90a2d5a27511
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-02-10 21:33:01 -0500

    Adjust description

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo "Building"
 export CONVOX_RACK=$INPUT_RACK
-echo $GITHUB_SHA
-echo $DESCRIPTION
+echo $GITHUB_SHA:8
+echo $GITHUB_SHA::8
+DESCRIPTION="${GITHUB_RUN_ID}:${GITHUB_RUN_NUMBER} ${GITHUB_SHA:8} ${GITHUB_SHA::8} ${GITHUB_ACTOR}"
 release=$(cd backend && convox build -d $DESCRIPTION --app $INPUT_APP --id)
 if [ -z "$release" ]
 then
