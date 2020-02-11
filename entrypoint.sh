dee9b6cad90c9f52e8a4d1dd9456199265f6f13e
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-02-10 22:03:33 -0500

    Interpolate string?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo "Building"
 export CONVOX_RACK=$INPUT_RACK
-SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-6)
-TAGS=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
+SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-8)
 BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g")
-DESCRIPTION="${BRANCH}#${SHORT_SHA} ${TAGS} - ${GITHUB_ACTOR}"
+DESCRIPTION="${BRANCH}#${SHORT_SHA} - ${GITHUB_ACTOR}"
 echo $DESCRIPTION
-release=$(cd backend && convox build --description $DESCRIPTION --app $INPUT_APP --id)
+release=$(cd backend && convox build --description "${DESCRIPTION}" --app $INPUT_APP --id)
 if [ -z "$release" ]
 then
   echo "Build failed"
