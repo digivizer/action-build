d4269102676a43967d6d4d71b078b133be31a793
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-02-10 21:50:12 -0500

    echo description

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo "Building"
 export CONVOX_RACK=$INPUT_RACK
-DESCRIPTION="${GITHUB_RUN_ID}:${GITHUB_RUN_NUMBER} ${GITHUB_SHA} ${GITHUB_ACTOR}\n${GITHUB_SHA} "
-release=$(cd backend && convox build -d $DESCRIPTION --app $INPUT_APP --id)
+DESCRIPTION="${GITHUB_RUN_ID}:${GITHUB_RUN_NUMBER} ${GITHUB_SHA} ${GITHUB_ACTOR}\n${GITHUB_SHA}"
+echo $DESCRIPTION
+release=$(cd backend && convox build --description $DESCRIPTION --app $INPUT_APP --id)
 if [ -z "$release" ]
 then
   echo "Build failed"
