65dd83ab8754b0c02ea51e2a473626dd2e6af959
Scott Ames-Messinger <scottmessinger@gmail.com>
2020-02-10 21:59:46 -0500

    Take carriage return out of description

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 echo "Building"
 export CONVOX_RACK=$INPUT_RACK
-DESCRIPTION="${GITHUB_RUN_ID}:${GITHUB_RUN_NUMBER} ${GITHUB_SHA} ${GITHUB_ACTOR}\n${GITHUB_SHA}"
+SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-6)
+TAGS=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
+BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g")
+DESCRIPTION="${BRANCH}#${SHORT_SHA} ${TAGS} - ${GITHUB_ACTOR}"
 echo $DESCRIPTION
 release=$(cd backend && convox build --description $DESCRIPTION --app $INPUT_APP --id)
 if [ -z "$release" ]
